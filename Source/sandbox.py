8ed8d309f8d068d710d69ce24e4d54e131298cf0
Edison Phua <108711622+EdisonPhua@users.noreply.github.com>
2023-02-10 16:41:12 +0000

    Feb 10, 2023, 4:41 PM

--- Source/sandbox.py
+++ Source/sandbox.py
@@ -1,7 +1,7 @@
 import os
 
 # Access the secret stored in Github Actions
-secret_value = os.environ.get(ServiceAccountKey)
+secret_value = os.environ.get('ServiceAccountKey')
 
 # Use the secret value in your code
 print(secret_value)

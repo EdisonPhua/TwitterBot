4262b376372169e65387f79d514d262ceba88512
Edison Phua <108711622+EdisonPhua@users.noreply.github.com>
2023-02-11 01:24:51 +0000

    updated sandbox

--- Source/sandbox.py
+++ Source/sandbox.py
@@ -4,6 +4,6 @@ import json
 # Access the secret stored in Github Actions
 secret_value = os.environ.get('ServiceAccountKey')
 
-
+#hello
 # Use the secret value in your code
-print(secret_value)
+print('Testing 123')

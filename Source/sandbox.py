da062df59d8d6e91c50d1d13b38e9d52e2f66bc4
Edison Phua <108711622+EdisonPhua@users.noreply.github.com>
2023-02-12 15:43:53 +0000

    Sun, Feb 12, 2023, 3:43 PM +00:00

--- Source/sandbox.py
+++ Source/sandbox.py
@@ -1,5 +1,9 @@
+import requests
+import json
 import functions
-
+import nltk
+nltk.download('punkt')
+from nltk import sent_tokenize
 
 print('Time to generate new articles')
 exec(open("Source/NewsData.py").read())

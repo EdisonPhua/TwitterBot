dfaef17c61f73d9202834a241c52fcd68abc101e
Edison Phua <108711622+EdisonPhua@users.noreply.github.com>
2023-02-12 16:26:02 +0000

    Sun, Feb 12, 2023, 4:26 PM +00:00

--- Source/sandbox.py
+++ Source/sandbox.py
@@ -5,10 +5,6 @@ import nltk
 nltk.download('punkt')
 from nltk import sent_tokenize
 
-print('Time to generate new articles')
-exec(open("Source/NewsData.py").read())
-print('Generated')  
-
 tweet, link = functions.NewsLooper2()
 
 text = sent_tokenize(tweet)[:5]

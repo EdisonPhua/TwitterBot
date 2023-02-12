33f647b392d68fe69f1da6291dc34c2055feaf31
Edison Phua <108711622+EdisonPhua@users.noreply.github.com>
2023-02-12 15:43:12 +0000

    Sun, Feb 12, 2023, 3:43 PM +00:00

--- Source/sandbox.py
+++ Source/sandbox.py
@@ -1,1 +1,14 @@
 import functions
+
+
+print('Time to generate new articles')
+exec(open("Source/NewsData.py").read())
+print('Generated')  
+
+tweet, link = functions.NewsLooper()
+
+text = sent_tokenize(tweet)[:5]
+text = ' '.join(text)
+count = 0
+
+print(text)

7d90b3542b2f64e869ad26ecc980fcad18b948c6
EdisonPhua <edison.phua2004@gmail.com>
2023-02-15 16:22:24 +0000

    Thu, Feb 16, 2023, 12:22 AM +08:00

--- Source/NewsData.py
+++ Source/NewsData.py
@@ -26,7 +26,7 @@ while True:
     for i in range(response['totalResults']):    
         try:
             text = response['results'][i]['content']
-        except ValueError:
+        except ValueError or IndexError:
             nextpage = response['nextPage']
             response = api.news_api( country = 'us,cn,jp,kr,de', category='technology,science,business,top', language='en', page=nextpage  )
             continue

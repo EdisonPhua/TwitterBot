c9056c0a705e1bfe05ff8cc9903ad8d8b2eec5f2
Edison Phua <108711622+EdisonPhua@users.noreply.github.com>
2023-02-16 14:44:42 +0000

    Thu, Feb 16, 2023, 2:44 PM +00:00

--- Source/sandbox.py
+++ Source/sandbox.py
@@ -1,6 +1,13 @@
-while True:
-    bruh = str(input('enter:'))
-    if bruh == 'stop':
-        break
-    else:
-        continue
+import requests
+import json
+import functions
+import nltk
+nltk.download('punkt')
+from nltk import sent_tokenize
+
+
+tweet, link = functions.NewsLooper()
+
+print(tweet)
+print(link)
+    

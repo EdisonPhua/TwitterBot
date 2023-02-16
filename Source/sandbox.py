fadb162791b6124f8a16a557b8ec716c982094c9
Edison Phua <108711622+EdisonPhua@users.noreply.github.com>
2023-02-16 09:22:41 +0000

    Thu, Feb 16, 2023, 9:22 AM +00:00

--- Source/sandbox.py
+++ Source/sandbox.py
@@ -1,25 +1,6 @@
-import functions
-import json
-from newsdataapi import NewsDataApiClient
-import firebase_admin
-from firebase_admin import credentials, db 
-import os
-
-
-secret_value = os.environ.get('SERVICEACCOUNTKEY')
-secret_value = json.loads(secret_value)
-
-cred = credentials.Certificate(secret_value)
-database_url = secret_value["databaseURL"]
-try:
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': database_url})
-except ValueError:
-    pass 
-ref = db.reference('OpenAI/')
-users_ref = ref.child('NewsData')
-Key = users_ref.get()['API']
-api = NewsDataApiClient(apikey=Key)
-response = api.news_api( country = 'us,cn,jp,kr,de', category='technology,science', language='en'  )
-count = 0
-print(response['totalResults'])
+while True:
+    input = str(input('enter:'))
+    if input == 'stop':
+        break
+    else:
+        continue

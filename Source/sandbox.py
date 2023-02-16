c0e9a5ebb7d87bea481461c5d3e231db1e6eb84a
Edison Phua <108711622+EdisonPhua@users.noreply.github.com>
2023-02-16 08:31:34 +0000

    Thu, Feb 16, 2023, 8:31 AM +00:00

--- Source/sandbox.py
+++ Source/sandbox.py
@@ -1,13 +1,25 @@
-import requests
-import json
 import functions
-import nltk
-nltk.download('punkt')
-from nltk import sent_tokenize
+import json
+from newsdataapi import NewsDataApiClient
+import firebase_admin
+from firebase_admin import credentials, db 
+import os
 
 
-tweet, link = functions.NewsLooper()
+secret_value = os.environ.get('SERVICEACCOUNTKEY')
+secret_value = json.loads(secret_value)
 
-print(tweet)
-print(link)
-    
+cred = credentials.Certificate(secret_value)
+database_url = secret_value["databaseURL"]
+try:
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': database_url})
+except ValueError:
+    pass 
+ref = db.reference('OpenAI/')
+users_ref = ref.child('NewsData')
+Key = users_ref.get()['API']
+api = NewsDataApiClient(apikey=Key)
+response = api.news_api( country = 'us,cn,jp,kr,de', category='technology,science', language='en'  )
+count = 0
+print(response['totalResults'])

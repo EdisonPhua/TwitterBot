9f3158f7c04ba77608ba81cc58a5534595d4c706
Edison Phua <108711622+EdisonPhua@users.noreply.github.com>
2023-05-18 05:08:46 +0000

    Thu, May 18, 2023, 5:08 AM +00:00

--- Source/sandbox.py
+++ Source/sandbox.py
@@ -1,5 +1,31 @@
-import functions 
+import functions
+import json
+from newsdataapi import NewsDataApiClient
+import firebase_admin
+from firebase_admin import credentials, db 
+import os
 
-text, link = functions.NewsLooper()
+secret_value = os.environ.get('SERVICEACCOUNTKEY')
+secret_value = json.loads(secret_value)
 
-print(text, link)
+cred = credentials.Certificate(secret_value)
+database_url = secret_value["databaseURL"]
+try:
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': database_url})
+except ValueError:
+    pass 
+ref = db.reference('OpenAI/')
+users_ref = ref.child('NewsData')
+Key = users_ref.get()['API']
+api = NewsDataApiClient(apikey=Key)
+response = api.news_api( country = 'us,cn,jp,kr,de', category='technology,science', language='en'  )
+count = 0
+errors = 0
+print(response)
+text = response['results'][i]['content']
+print(text)
+
+    
+    
+

84808565930049ceb64e400140ad75ab9d46e99f
Edison Phua <108711622+EdisonPhua@users.noreply.github.com>
2023-05-18 04:56:20 +0000

    Thu, May 18, 2023, 4:56 AM +00:00

--- Source/NewsData.py
+++ Source/NewsData.py
@@ -5,12 +5,6 @@ import firebase_admin
 from firebase_admin import credentials, db 
 import os
 
-
-
-
-
-
-
 secret_value = os.environ.get('SERVICEACCOUNTKEY')
 secret_value = json.loads(secret_value)
 

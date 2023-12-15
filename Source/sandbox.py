3bd76ac480228ab36e764361e8d8ad8be3b9581a
EdisonPhua <edison.phua2004@gmail.com>
2023-12-15 21:07:58 +0800

    Changed sandbox to display time

--- Source/sandbox.py
+++ Source/sandbox.py
@@ -1,3 +1,7 @@
-intro = "Hello World"
+# import time module
+import time
 
-print(f"Hello {intro}!")
+# For i in range of 10 print the time
+for i in range(10):
+    print(time.ctime())
+    time.sleep(2)
